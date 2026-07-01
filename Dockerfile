cae679357aa08ea7cc18d65b2497e7cfff67dd1f
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-07-01 14:34:23 +0200

    Deploy til nais

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM europe-north1-docker.pkg.dev/cgr-nav/pull-through/nav.no/node:24-slim
+
+WORKDIR usr/src/app
+
+COPY ./dist ./dist
+COPY ./node_modules ./node_modules
+
+ENV HOST=0.0.0.0
+ENV PORT=3000
+
+CMD ["./dist/server/entry.mjs"]
+
+EXPOSE $PORT
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
+  base: '/produktside-prototype',
   redirects: {
     '/': '/dagpenger',
   },
